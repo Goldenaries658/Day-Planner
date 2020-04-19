6aafdfa68c578bfa108a6b25ae157e27e0a02a5c
Goldenaries <jacob.houston658@gmail.com>
2020-04-19 15:33:37 +0100

    added save functionality

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(function(){
-    
+$(function () {
+  setInterval(function () {
+    $('#current-date').text(moment().format('MMMM Do YYYY, H:mm:ss'));
+  }, 500);
+
+  // Saving user input
+  $('button[type="save"]').on('click', function (event) {
+    var clickedButton = event.target;
+    var inputNumber = $(clickedButton).attr('row-number');
+    var input = '#event-input' + inputNumber;
+    var output = '#event' + inputNumber;
+    var inputEvent = $(input).val();
+
+    localStorage.setItem(inputNumber, inputEvent);
+    $(output).text(inputEvent);
+    $(input).hide();
+    $(output).show();
+  });
+
+  // Editing saved input
+  $('button[type="edit"]').on('click', function (event){
+      
+  });
 });
