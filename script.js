2e957fa5ac05c1f177582a997b6e8f74ffada913
Goldenaries <jacob.houston658@gmail.com>
2020-04-19 16:12:00 +0100

    Displaying localStorage on page load

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 $(function () {
+  // Displaying current date/time
   setInterval(function () {
     $('#current-date').text(moment().format('MMMM Do YYYY, H:mm:ss'));
   }, 500);
 
+  // Displaying previous inputs
+  for (i = 0; i < 24; i++) {
+    var input = '#event-input' + i;
+    console.log(input);
+    var output = '#event' + i;
+    console.log(output);
+    var savedInput = localStorage.getItem(i);
+
+    if (!savedInput) {
+      $('button[row-number=' + i + '][type="edit"]').hide();
+    } else {
+      $('button[row-number=' + i + '][type="save"]').hide();
+      $(input).hide();
+      $(output).text(savedInput);
+      $(output).show();
+    }
+  }
+  
   // Saving user input
   $('button[type="save"]').on('click', function (event) {
     var clickedButton = event.target;
@@ -18,7 +37,15 @@ $(function () {
   });
 
   // Editing saved input
-  $('button[type="edit"]').on('click', function (event){
-      
+  $('button[type="edit"]').on('click', function (event) {
+    var clickedButton = event.target;
+    var inputNumber = $(clickedButton).attr('row-number');
+    var input = '#event-input' + inputNumber;
+    var output = '#event' + inputNumber;
+    var eventText = $(output).text();
+
+    $(output).hide();
+    $(input).val(eventText);
+    $(input).show();
   });
 });
